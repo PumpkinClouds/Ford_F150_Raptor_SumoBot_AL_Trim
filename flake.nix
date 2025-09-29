44f54e91062592bf96233ee14fcb638b4c48450b
samminhch <45446622+samminhch@users.noreply.github.com>
2025-09-29 18:01:45 -0500

    refactor: adding some dev files and reorganizing file structure

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A quick flake for STM32 development";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs =
+    inputs@{ self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      devShells."${system}".default =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        pkgs.mkShell {
+          packages = with pkgs; [
+            kicad
+            platformio
+          ];
+
+          shellHook = ''
+            echo "You're in the STM32 Project
+          '';
+        };
+    };
+}
+
